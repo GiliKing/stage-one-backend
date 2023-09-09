20406187df2922df3053d53f3ce4952304519d81
giliking <chrisogili12@gmail>
2023-09-09 15:37:26 -0700

    we finally made it

--- api.php
+++ api.php
@@ -1,35 +1,21 @@
-
 <?php
-
-
-
+// Set CORS headers first
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json; charset=UTF-8");
-
 
 include_once "./class/data.php";
 
-
-if(isset($_GET['slack_name']) && isset($_GET['track'])) {
-
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
     $data = new Christian;
-
     $slack_name = trim($_GET['slack_name'], "\"'");
     $track = trim($_GET['track'], "\"'");
-
     $data->setItem($slack_name, $track);
-
-    http_response_code(200);    
-    echo json_encode($data->getItem());
-
-
+    http_response_code(200); 
+    echo json_encode($data->getItem(), JSON_UNESCAPED_UNICODE);
 } else {
-
-
-    http_response_code(404);     
-    echo json_encode(
-        array("message" => "No item found.")
-    );
-
-
+    http_response_code(404);
+    echo json_encode(array("message" => "No item found."), JSON_UNESCAPED_UNICODE);
 }
+
+
--- class/data.php
+++ class/data.php
@@ -1,8 +1,8 @@
-
-
 <?php
 
 // create a class for my data
+// header("Access-Control-Allow-Origin: *");
+// header("Content-Type: application/json");
 
 class Christian {
 
@@ -16,12 +16,26 @@ class Christian {
 
     public function setItem($slack_name, $track)
     {
+        // Get the current UTC time
+        $currentTimeUTC = gmdate('Y-m-d H:i:s');
+
+        // Get the current UTC timestamp
+        $currentTimeStamp = strtotime($currentTimeUTC);
+
+        // Get the current UTC timestamp +/- 2 seconds
+        $allowedTimeRange = 2; // You can adjust this value as needed
+        $minTimestamp = $currentTimeStamp - $allowedTimeRange;
+        $maxTimestamp = $currentTimeStamp + $allowedTimeRange;
+
+        // Get the current UTC time within the allowed range
+        $currentTimeWithinRange = gmdate('Y-m-d\TH:i:s\Z', rand($minTimestamp, $maxTimestamp));
+
         $this->slack_name = $slack_name;
         $this->track = $track;
         $this->current_day = date('l');
-        $this->utc = gmdate('Y-m-d H:i:s');
-        $this->github_file_url = "1 for now";
-        $this->github_repo_url = "2 for now";
+        $this->utc = $currentTimeWithinRange;
+        $this->github_file_url = "https://github.com/GiliKing/stage-one-backend/blob/master/api.php";
+        $this->github_repo_url = "https://github.com/GiliKing/stage-one-backend/";
         $this->status_code = 200;
     }
 
@@ -33,7 +47,7 @@ class Christian {
 
         $data_christian->slack_name = $this->slack_name;
         $data_christian->current_day = $this->current_day;
-        $data_christian->utc = $this->utc;
+        $data_christian->utc_time = $this->utc;
         $data_christian->track = $this->track;
         $data_christian->github_file_url = $this->github_file_url;
         $data_christian->github_repo_url = $this->github_repo_url;
